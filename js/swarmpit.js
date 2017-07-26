d1eb398e10f28229f722181a6b2189f79c559d37
Aleš Roubíček <ales@roubicek.name>
2017-07-26 08:50:58 +0200

    Performance optimizations
    Load styles and script from CDN with SRI. Preload assets. Defer scripts.

--- js/swarmpit.js
+++ js/swarmpit.js
@@ -1,5 +1,5 @@
 (function($) {
-    "use strict"; // Start of use strict
+    "use strict";
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $(document).on('click', 'a.page-scroll', function(event) {
@@ -9,8 +9,6 @@
         }, 1250, 'easeInOutExpo');
         event.preventDefault();
     });
-
- 
 
     // Closes the Responsive Menu on Menu Item Click
     $('.navbar-collapse ul li a').click(function() {
@@ -22,6 +20,21 @@
         offset: {
             top: 50
         }
-    })
+    });
 
+    $(function () {
+        $("#postcontentsb").submit(function (e) {
+            e.preventDefault();
+            var data = {};
+            $(this).serializeArray().map(function(x){data[x.name] = x.value;});
+
+            fetch("https://fokgz5bqh2.execute-api.eu-central-1.amazonaws.com/swarmpit/mailing-list", {
+                method: "POST",
+                body: JSON.stringify(data)
+            }).then(function() {
+                $('.email-wrapper').html("<p>Thank you for submitting!</p>");
+            });
+            e.preventDefault();
+        });
+    });
 })(jQuery); // End of use strict
