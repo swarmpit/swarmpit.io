9886c0705a63c4b8c5eefc416330ff130e74a352
Aleš Roubíček <ales@roubicek.name>
2017-08-18 16:00:20 +0200

    Custom bundle of Bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
 var cssnano = require("cssnano");
 var autoprefixer = require("autoprefixer");
 var postcss = require('gulp-postcss');
@@ -8,12 +9,13 @@ var htmlmin = require("gulp-htmlmin");
 var rename = require("gulp-rename");
 var size = require("gulp-size");
 var uglify = require('gulp-uglify');
+var path = require('path');
 
 // Compile LESS files from /less into /css
 gulp.task('less', function () {
     return gulp.src('src/less/swarmpit.less')
-        .pipe(less())
-        .pipe(gulp.dest('s3/swarmpit.io/css'))
+        .pipe(less({ paths: [path.join(__dirname, 'node_modules')] }))
+        .pipe(gulp.dest('src/css'))
         .pipe(browserSync.reload({
             stream: true
         }))
@@ -31,7 +33,7 @@ gulp.task('minify-css', ['less'], function () {
             zindex: false
         })
     ];
-    return gulp.src('s3/swarmpit.io/css/swarmpit.css')
+    return gulp.src('src/css/swarmpit.css')
         .pipe(postcss(plugins))
         .pipe(rename({ suffix: '.min' }))
         .pipe(size({ title: "CSS", gzip: true }))
@@ -43,7 +45,8 @@ gulp.task('minify-css', ['less'], function () {
 
 // Minify JS
 gulp.task('minify-js', function () {
-    return gulp.src('src/js/swarmpit.js')
+    return gulp.src(['node_modules/bootstrap/js/affix.js', 'node_modules/bootstrap/js/collapse.js', 'src/js/swarmpit.js'])
+        .pipe(concat('swarmpit.js'))
         .pipe(uglify())
         .pipe(rename({ suffix: '.min' }))
         .pipe(size({ title: "JS", gzip: true }))
@@ -54,22 +57,22 @@ gulp.task('minify-js', function () {
 });
 
 gulp.task('minify-html', function () {
-   return gulp.src('s3/swarmpit.io/index.html')
-       .pipe(htmlmin({
-           collapseWhitespace: true,
-           collapseBooleanAttributes: true,
-           decodeEntities: true,
-           minifyCSS: true,
-           minifyJS: true,
-           removeAttributeQuotes: true,
-           removeComments: true,
-           removeOptionalTags: true,
-           removeRedundantAttributes: true,
-           removeScriptTypeAttributes: true,
-           removeStyleLinkTypeAttributes: true
-       }))
-       .pipe(size({ title: "HTML", gzip: true }))
-       .pipe(gulp.dest('s3/swarmpit.io'))
+    return gulp.src('s3/swarmpit.io/index.html')
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            collapseBooleanAttributes: true,
+            decodeEntities: true,
+            minifyCSS: true,
+            minifyJS: true,
+            removeAttributeQuotes: true,
+            removeComments: true,
+            removeOptionalTags: true,
+            removeRedundantAttributes: true,
+            removeScriptTypeAttributes: true,
+            removeStyleLinkTypeAttributes: true
+        }))
+        .pipe(size({ title: "HTML", gzip: true }))
+        .pipe(gulp.dest('s3/swarmpit.io'))
 });
 
 // Run everything
@@ -85,11 +88,13 @@ gulp.task('browserSync', function () {
 });
 
 // Dev task with browserSync
-gulp.task('default', ['browserSync', 'minify-css', 'minify-js'], function () {
+gulp.task('watch', ['minify-css', 'minify-js'], function () {
     gulp.watch('src/less/*.less', ['less']);
-    gulp.watch('s3/swarmpit.io/css/*.css', ['minify-css']);
+    gulp.watch('src/css/*.css', ['minify-css']);
     gulp.watch('src/js/*.js', ['minify-js']);
     // Reloads the browser whenever HTML or JS files change
     gulp.watch('s3/swarmpit.io/*.html', browserSync.reload);
     gulp.watch('s3/swarmpit.io/js/**/*.js', browserSync.reload);
 });
+
+gulp.task('default', ['watch', 'browserSync']);
